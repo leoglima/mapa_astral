cfbf6377b0b1fd950e8b53ab4e232e7ffd710b52
Leonardo Lima <leonardo.glima@live.com>
2020-12-26 01:49:21 -0300

    Alterações de rota

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from '@material-ui/core';
+import HomeIcon from '@material-ui/icons/Home';
+import InfoIcon from '@material-ui/icons/Info';
+const useStyles = makeStyles((theme) => ({
+  drawerPaper: { width: 'inherit' },
+  link: { textDecoration: 'none', color: theme.palette.text.primary },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Router>
+      <div style={{ display: 'flex' }}>
+        <Drawer
+          style={{ width: '220px' }}
+          variant="persistent"
+          anchor="left"
+          open={true}
+          classes={{ paper: classes.drawerPaper }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <List>
+            <Link to="/" className={classes.link}>
+              <ListItem button>
+                <ListItemIcon>
+                  <HomeIcon />
+                </ListItemIcon>
+                <ListItemText primary={'Home'} />
+              </ListItem>
+            </Link>
+            <Link to="/about" className={classes.link}>
+              <ListItem button>
+                <ListItemIcon>
+                  <InfoIcon />
+                </ListItemIcon>
+                <ListItemText primary={'About'} />
+              </ListItem>
+            </Link>
+          </List>
+        </Drawer>
+        <Switch>
+          <Route exact path="/">
+            Home
+          </Route>
+          <Route exact path="/about">
+            About
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
