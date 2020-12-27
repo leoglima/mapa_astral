5efd744e7d688121e6f0f1839bf3da5524523c27
Leonardo Lima <leonardo.glima@live.com>
2020-12-27 03:35:10 -0300

    Adicionado o botão de cadastrar

--- src/templates/Cadastro.js
+++ src/templates/Cadastro.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
+
 import Timer from './assets/Timer';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
-      width: '250px',
-    },
-  },
-}));
-
 export default function BasicTextFields() {
-  const classes = useStyles();
-
   return (
-    <form className={classes.root} noValidate autoComplete="off">
-      <TextField id="standard-basic" label="Nome" />
+    <div>
       <Timer />
-    </form>
+    </div>
   );
 }
--- src/templates/assets/Timer.js
+++ src/templates/assets/Timer.js
@@ -1,51 +1,86 @@
 import 'date-fns';
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import DateFnsUtils from '@date-io/date-fns';
 import {
   MuiPickersUtilsProvider,
+  DateTimePicker,
   KeyboardTimePicker,
   KeyboardDatePicker,
 } from '@material-ui/pickers';
+import { Container } from '@material-ui/core';
 
 export default function MaterialUIPickers() {
-  // The first commit of Material-UI
-  const [selectedDate, setSelectedDate] = React.useState(
-    new Date('2014-08-18T21:11:54'),
-  );
+  const [selectedDate, setSelectedDate] = React.useState(new Date());
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
   };
 
   return (
-    <MuiPickersUtilsProvider utils={DateFnsUtils}>
-      <Grid container justify="space-around">
-        <KeyboardDatePicker
-          disableToolbar
-          variant="inline"
-          format="MM/dd/yyyy"
-          margin="normal"
-          id="date-picker-inline"
-          label="Date picker inline"
-          value={selectedDate}
-          onChange={handleDateChange}
-          KeyboardButtonProps={{
-            'aria-label': 'change date',
-          }}
-        />
-
-        <KeyboardTimePicker
-          margin="normal"
-          id="time-picker"
-          label="Time picker"
-          value={selectedDate}
-          onChange={handleDateChange}
-          KeyboardButtonProps={{
-            'aria-label': 'change time',
-          }}
-        />
-      </Grid>
-    </MuiPickersUtilsProvider>
+    <>
+      <Container maxWidth="xl">
+        <Grid container spacing={1}>
+          <Grid item xs={12}>
+            <TextField
+              id="standard-basic"
+              label="Nome"
+              margin="normal"
+              fullWidth
+            />
+          </Grid>
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <Grid item xs={3}>
+              <KeyboardDatePicker
+                disableToolbar
+                variant="inline"
+                format="dd/MM/yyyy"
+                margin="normal"
+                id="date-picker-inline"
+                label="Data de nascimento"
+                value={selectedDate}
+                onChange={handleDateChange}
+                KeyboardButtonProps={{
+                  'aria-label': 'change date',
+                }}
+              />
+            </Grid>
+            <Grid item xs={3}>
+              <KeyboardTimePicker
+                margin="normal"
+                id="time-picker"
+                label="Hora de nascimento"
+                value={selectedDate}
+                onChange={handleDateChange}
+                KeyboardButtonProps={{
+                  'aria-label': 'change time',
+                }}
+              />
+            </Grid>
+            <Grid item xs={3}>
+              <TextField
+                id="standard-basic"
+                label="Cidade"
+                margin="normal"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={3}>
+              <TextField
+                id="standard-basic"
+                label="Estado"
+                margin="normal"
+                fullWidth
+              />
+            </Grid>
+          </MuiPickersUtilsProvider>
+        </Grid>
+        <Button variant="contained" color="primary">
+          Cadastrar
+        </Button>
+      </Container>
+    </>
   );
 }
